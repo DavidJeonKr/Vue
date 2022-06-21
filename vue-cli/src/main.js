183a5684e2c466e67e09afac44b831d3b9c641bc
DavidJeonKr <dudgh7410@naver.com>
2022-06-21 18:03:53 +0900

    router 추가

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import router from './router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App).use(router).mount('#app')
+// const app = createApp({
+//     data() {
+//         return {
+//             count: 0
+//         }
+//     }
+// }).use(router)
+// app.mount('#app')
+
