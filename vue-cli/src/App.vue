646ccf09127b57757f2b05e1a4532d5a29e824cb
DavidJeonKr <dudgh7410@naver.com>
2022-06-21 16:03:45 +0900

    뷰 환경 설정 완료

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="nice to meet you"/>
+  <div>
+    <a href="www.naver.com">네이버</a>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
